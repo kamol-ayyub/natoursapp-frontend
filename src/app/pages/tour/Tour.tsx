e9251af3e068de3fbcbc53be45661975f4226b0a
ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2022-09-07 15:50:40 +0500

    tour header changed with dynamic data

--- src/app/pages/tour/Tour.tsx
+++ src/app/pages/tour/Tour.tsx
@@ -1,21 +1,33 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { Cta } from '../../../components/cta/Cta';
 import { TourHeader } from '../../../components/tour-header/TourHeader';
 import Overview from '../../../components/overview/Overview';
 import { Review } from '../../../components/review/Review';
+import useHttp from '../../../hooks/use-http';
 
 const Tour: FC = () => {
   const { tour } = useParams();
-  console.log(tour);
+  const { response, sendRequest: getTour, isError } = useHttp();
+  const sendRequest = async () => {
+    await getTour({
+      url: `/api/v1/tours/${tour}`,
+      method: 'GET',
+    });
+  };
+  useEffect(() => {
+    sendRequest();
+  }, [tour]);
+
+  if (isError) return <h1>SOmething went wrong! Try again!</h1>;
   return (
-    <div>
-      <TourHeader />
+    <>
+      <TourHeader data={response?.data?.data} />
       <Overview />
       <Review />
       <Cta />
-    </div>
+    </>
   );
 };
 
--- src/components/tour-header/TourHeader.tsx
+++ src/components/tour-header/TourHeader.tsx
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { GoLocation } from 'react-icons/go';
+import { BsClock } from 'react-icons/bs';
 
-export const TourHeader = () => {
+type HeaderProps = any;
+
+export const TourHeader: FC<HeaderProps> = ({ data }) => {
+  console.log(data);
   return (
     <section className='section-header'>
+      <div className='header__hero'>
+        <div className='header__hero-overlay'>&nbsp;</div>
+        <img
+          src={require(`../../img/tours/${data?.imageCover}`)}
+          alt={`${data?.imageCover}`}
+          className='header__hero-img'
+        />
+      </div>
       <div className='heading-box'>
         <h1 className='heading-primary'>
-          <span>
-            The Park <br />
-            Camper Tour
-          </span>
+          <span>{data?.name}</span>
         </h1>
         <div className='heading-box__group'>
           <div className='heading-box__detail'>
-            <svg className='heading-box__icon'>
-              <use xlinkHref='img/icons.svg#icon-clock'></use>
-            </svg>
-            <span className='heading-box__text'>10 days</span>
+            <BsClock />
+            <span className='heading-box__text'>{data?.duration}</span>
           </div>
           <div className='heading-box__detail'>
-            <svg className='heading-box__icon'>
-              <use xlinkHref='img/icons.svg#icon-map-pin'></use>
-            </svg>
-            <span className='heading-box__text'>Las Vegas, USA</span>
+            <GoLocation />
+            <span className='heading-box__text'>
+              {data?.startLocation.description}
+            </span>
           </div>
         </div>
       </div>
