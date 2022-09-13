bf6b63a606f2c6f0fceb3403a979b23a82f4f31a
ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2022-09-13 17:41:47 +0500

    toastify added to chane password component

--- src/components/account-view/AccountView.tsx
+++ src/components/account-view/AccountView.tsx
@@ -1,12 +1,17 @@
 import FormInput from '../input/EmailInput';
-import { useRef, FC } from 'react';
+import { useRef, FC, useEffect } from 'react';
 import useHttp from '../../hooks/use-http';
 import UserImg from '../../img/users/default.jpg';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface AccountViewProps {
   children: JSX.Element;
 }
 export const AccountView: FC<AccountViewProps> = ({ children }) => {
+  const notify = (text: string | boolean | null) => {
+    toast(text);
+  };
   const { response, sendRequest: changeNameAndEmail, isError } = useHttp();
   const token = localStorage.getItem('token');
 
@@ -14,8 +19,6 @@ export const AccountView: FC<AccountViewProps> = ({ children }) => {
   const nameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
   const photoRef = useRef<HTMLInputElement>(null);
-
-  console.log(token);
 
   // function for request to backend
   const handleSignup = async (event: any) => {
@@ -32,49 +35,62 @@ export const AccountView: FC<AccountViewProps> = ({ children }) => {
     });
     event.target.reset();
   };
-  return (
-    <div className='user-view__content'>
-      <div className='user-view__form-container'>
-        <h2 className='heading-secondary ma-bt-md'>Your account settings</h2>
-        <form onSubmit={handleSignup} className='form form-user-data'>
-          <div className='form__group'>
-            <FormInput
-              label='Name'
-              inputType='text'
-              formClass='form--signup ma-bt-md'
-              ref={nameRef}
-              required
-            />
-            <FormInput
-              label='Email address'
-              inputType='email'
-              formClass='form--signup ma-bt-md'
-              ref={emailRef}
-              required
-            />
-            <div className='form__group form__photo-upload'>
-              <img className='form__user-photo' src={UserImg} alt='user' />
-              <input
-                name='photo'
-                id='photo'
-                accept='image/*'
-                type='file'
-                className='form__upload'
-                ref={photoRef}
-              />
 
-              <label htmlFor='photo'>Choose new photo</label>
+  useEffect(() => {
+    if (response?.status === 'success') {
+      console.log(response);
+      notify('You successfully changed your name and email!');
+    } else if (response?.status === 'error') {
+      notify(isError);
+    }
+  }, [response]);
+
+  return (
+    <>
+      {<ToastContainer />}
+      <div className='user-view__content'>
+        <div className='user-view__form-container'>
+          <h2 className='heading-secondary ma-bt-md'>Your account settings</h2>
+          <form onSubmit={handleSignup} className='form form-user-data'>
+            <div className='form__group'>
+              <FormInput
+                label='Name'
+                inputType='text'
+                formClass='form--signup ma-bt-md'
+                ref={nameRef}
+                required
+              />
+              <FormInput
+                label='Email address'
+                inputType='email'
+                formClass='form--signup ma-bt-md'
+                ref={emailRef}
+                required
+              />
+              <div className='form__group form__photo-upload'>
+                <img className='form__user-photo' src={UserImg} alt='user' />
+                <input
+                  name='photo'
+                  id='photo'
+                  accept='image/*'
+                  type='file'
+                  className='form__upload'
+                  ref={photoRef}
+                />
+
+                <label htmlFor='photo'>Choose new photo</label>
+              </div>
+              <div className='form__group right'>
+                <button className='btn btn--small btn--green'>
+                  Save settings
+                </button>
+              </div>
             </div>
-            <div className='form__group right'>
-              <button className='btn btn--small btn--green'>
-                Save settings
-              </button>
-            </div>
-          </div>
-        </form>
+          </form>
+        </div>
+        <div className='line'>&nbsp;</div>
+        {children}
       </div>
-      <div className='line'>&nbsp;</div>
-      {children}
-    </div>
+    </>
   );
 };
