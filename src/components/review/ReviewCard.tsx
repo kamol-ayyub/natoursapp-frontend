34412bea4318b3f074a5bcaeaf0354b8c2801a76
ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2022-09-28 16:29:11 +0500

    review card updated

--- src/components/review/ReviewCard.tsx
+++ src/components/review/ReviewCard.tsx
@@ -1,21 +1,65 @@
+import { FC } from 'react';
 import { ReviewStar } from './ReviewStar';
 import { ReviewerType } from '../../types/types';
+import styled from 'styled-components';
+const ReviewCardBase = styled.div`
+  width: 30rem;
+  padding: 4rem;
+  background-color: #f7f7f7;
+  border-radius: 3px;
+  -webkit-box-shadow: 0 1.5rem 4rem rgba(0, 0, 0, 0.15);
+  box-shadow: 0 1.5rem 4rem rgba(0, 0, 0, 0.15);
+  scroll-snap-align: center;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+`;
+const ReviewAvatarBase = styled.div`
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  margin-bottom: 2rem;
+`;
+const ReviewAvatarImg = styled.img`
+  height: 4.5rem;
+  border-radius: 50%;
+  margin-right: 1.5rem;
+`;
 
-export const ReviewCard = ({ review, user, userPhoto }: ReviewerType) => {
+const ReviewsUser = styled.h6`
+  font-size: 1.5rem;
+  font-weight: 700;
+  text-transform: uppercase;
+`;
+const ReviewsText = styled.p`
+  font-size: 1.5rem;
+  margin-bottom: 2rem;
+  font-style: italic;
+  font-weight: 400;
+`;
+export const ReviewCard: FC<ReviewerType> = ({ review, user, userPhoto }) => {
   return (
-    <div className='reviews__card'>
-      <div className='reviews__avatar'>
-        <img
+    <ReviewCardBase>
+      <ReviewAvatarBase>
+        <ReviewAvatarImg
           src={require(`../../../public/img/users/${userPhoto}`)}
-          alt='Jim Brown'
-          className='reviews__avatar-img'
+          alt={`../../../public/img/users/${userPhoto}`}
         />
-        <h6 className='reviews__user'> {user} </h6>
-      </div>
-      <p className='reviews__text'>{review}</p>
-      <div className='reviews__rating'>
-        <ReviewStar />
-      </div>
-    </div>
+        <ReviewsUser> {user} </ReviewsUser>
+      </ReviewAvatarBase>
+      <ReviewsText>{review}</ReviewsText>
+
+      <ReviewStar />
+    </ReviewCardBase>
   );
 };
--- src/components/review/ReviewStar.tsx
+++ src/components/review/ReviewStar.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
 import { AiOutlineStar } from 'react-icons/ai';
 import { IconContext } from 'react-icons';
+import styled from 'styled-components';
+const ReviewsRating = styled.div`
+  margin-top: auto;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+`;
 export const ReviewStar = () => {
   return (
     <IconContext.Provider value={{ className: 'reviews__star--active' }}>
-      <AiOutlineStar />
-      <AiOutlineStar />
-      <AiOutlineStar />
-      <AiOutlineStar />
-      <AiOutlineStar />
+      <ReviewsRating>
+        <AiOutlineStar />
+        <AiOutlineStar />
+        <AiOutlineStar />
+        <AiOutlineStar />
+        <AiOutlineStar />
+      </ReviewsRating>
     </IconContext.Provider>
   );
 };
