f50c635cb084cdd9813b09ae8d0ff9dc6473cd94
Ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2023-01-30 08:56:29 +0500

    login  page has been updated

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,4 +1,4 @@
-import { FC, useRef, useEffect, useContext, useState } from 'react';
+import { FC, useEffect, useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useHttp from '@/hooks/use-http';
 import {
@@ -12,31 +12,39 @@ import {
   Button,
   RouteLink,
 } from '@/components';
-import { EmailAndPasswordType, FormEventType } from '@/types/types';
+import { FormEventType, OnChangeType } from '@/types/types';
 import { UserIsLoggedContext } from '@/context/Context';
 
 export const Login: FC = () => {
   const [message, setMessage] = useState<string>('');
   const [showForgotPassword, setShowForgotPassword] = useState<boolean>(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
   const { setLogged } = useContext(UserIsLoggedContext);
   const navigate = useNavigate();
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
+
   //
   const { response, sendRequest: sendrequestToLogin } = useHttp();
-  const formRef = useRef<HTMLFormElement>(null);
 
   const handleLogin = async (e: FormEventType) => {
     e.preventDefault();
-    const email: EmailAndPasswordType = emailRef.current?.value;
-    const password: EmailAndPasswordType = passwordRef.current?.value;
+    const { email, password } = formData;
 
     await sendrequestToLogin({
       url: '/api/v1/users/login',
       method: 'POST',
       data: { email, password },
     });
-    formRef.current?.reset();
+    setFormData({
+      email: '',
+      password: '',
+    });
+  };
+
+  const handleFormData = (e: OnChangeType) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   useEffect(() => {
@@ -68,22 +76,24 @@ export const Login: FC = () => {
             />
           )}
 
-          <Form formRef={formRef} submitForm={handleLogin}>
+          <Form submitForm={handleLogin}>
             <FormInput
               label='Email address'
               placeholder='you@example.com'
               inputType='email'
-              ref={emailRef}
               required
+              HTMLFor='email'
+              onchange={handleFormData}
             />
 
             <FormInput
               label='Password'
-              ref={passwordRef}
               placeholder='••••••••'
               inputType='password'
               MaBtMd
               required
+              HTMLFor='password'
+              onchange={handleFormData}
             />
 
             <FormGroup>
